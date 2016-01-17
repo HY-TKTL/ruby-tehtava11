a66c539ccf3d5514bc2fa5bab0e9de40d7e23377
Janika Kääriäinen <janika.kaariainen@helsinki.fi>
2016-01-17 20:13:04 +0200

    valmis

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
 # kirjoita koodi tänne
+
+def arvojarjestys(hash)
+  values = hash.values
+  values = values.sort
+  values = values.reject{|a| a<=0}
+  return values
+end
+
+puts arvojarjestys({1 => 9, 3 => 5})

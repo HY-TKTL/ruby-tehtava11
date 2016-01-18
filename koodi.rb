85fd9d85258793ebbd8ef88cc28356d99d5c41f8
perrymann <perttu.nikander@cs.helsinki.fi>
2016-01-18 12:50:11 +0200

    11. tehtävän palautus

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,8 @@
 # kirjoita koodi tänne
+def arvojarjestys(h)
+   taulukko = []
+  h.each_value {|i| unless i <= 0
+                      taulukko.push(i)
+                    end}
+  taulukko.sort!
+end

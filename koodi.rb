35775b9668c8184a4d60b8e2543c50bdc6a58b96
Arttu Ylhävuori <areee@users.noreply.github.com>
2016-01-20 21:26:47 +0200

    11. teht ratkaistu

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
 # kirjoita koodi tänne
+
+def arvojarjestys(hash)
+  taulukko = Array.new()
+  hash.each_value do |value|
+    if(value>0)
+    taulukko.push value
+    end
+  end
+  taulukko.sort!
+  return taulukko
+end
+
+puts arvojarjestys({7=>2, 4=>4, 3=>-2, 1=>1})

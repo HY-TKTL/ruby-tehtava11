871b683c50016fd7753d9159486106527773a666
FummiTaksi <mustonen.aleksi@kolumbus.fi>
2016-01-21 14:04:11 +0200

    Ei tunnu hyvältä

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,7 @@
 # kirjoita koodi tänne
+def arvojarjestys(mappi)
+  mappi.values.reject{|a| a < 0}.sort
+end
+map = {"Chang" => 5, "Pihla" => 4, "Doge" => -1000}
+arvojarjestys map
+puts arvojarjestys map

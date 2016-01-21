6f037b8d15862f613096bf49c6d94d3c284612c8
SusiSusi <suvi.sipila@helsinki.fi>
2016-01-21 15:50:24 +0200

    ykstoista tehty

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,9 @@
 # kirjoita koodi tänne
+def arvojarjestys(hash)
+	taulukko = []
+	taulukko = hash.values.sort
+	return taulukko.reject{ |a| a < 0}
+end
+
+hash = {3 => 2, 1 => 1, 9 => 8, 8 => 5}
+puts arvojarjestys(hash)

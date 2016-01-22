d0d592d93d5cab4efe4103865ea8cd44efa5b698
AnteronGitHub <antero.vainio@outlook.com>
2016-01-22 15:23:35 +0200

    koodi päivitetty

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
 # kirjoita koodi tänne
+def arvojarjestys(h)
+	ordered = []
+	h.each_value do |value|
+		if value > 0
+			i = 0
+			while (ordered[i] != nil && value > ordered[i]) do
+				i += 1
+			end
+			ordered.insert(i, value)
+		end
+	end
+	ordered
+end

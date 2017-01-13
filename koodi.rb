0dfbdd5580a871a46282e0fda327aad45b37b2a5
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-01-13 11:22:38 +0200

    opin miten mapin avaimed voidaan sortata, sekä montaa metodia voidaan käyttää samaan aikaan.

--- koodi.rb
+++ koodi.rb
@@ -1,7 +1,6 @@
-# kirjoita koodi tänne
+
 def arvojarjestys(mappi)
   mappi.values.reject{|a| a < 0}.sort
 end
 map = {"Chang" => 5, "Pihla" => 4, "Doge" => -1000}
-arvojarjestys map
 puts arvojarjestys map

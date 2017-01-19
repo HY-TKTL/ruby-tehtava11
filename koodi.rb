df62d97a201451f16fdc75f09a7e7ea26e83fc1f
Tiia Björk <tiia.bjork@gmail.com>
2017-01-19 22:37:18 +0200

    teht 11 palautus

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,9 @@
 # kirjoita koodi tänne
+def arvojarjestys(hash)
+  taulukko = []
+  hash.each_key { |key| taulukko.push(hash[key]) if hash[key] > 0}
+  return taulukko.sort
+end
+
+hash = Hash["a"=>4, "b"=>2, "c"=>8, "d"=>4, "e"=>8, "f"=>5, "g"=> -4]
+puts arvojarjestys(hash)

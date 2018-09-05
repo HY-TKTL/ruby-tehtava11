9df0eaf31b382c197c23f049e9ddad5ef8b4cbb3
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-09-05 19:35:00 +0300

    Paikalliset testit läpi

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,8 @@
-# kirjoita koodi tänne
+def arvojarjestys(h)
+    arvot = h.values.select {|x| x > 0}
+    arvot.sort
+  end
+  
+  test_input = {a:30, b:100, c:-10, d:40, e:10}
+  
+  puts arvojarjestys(test_input)
